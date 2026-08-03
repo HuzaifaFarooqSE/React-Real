df5d96c32e0d12bfa788732e9b68deebd32da742
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2026-08-03 09:19:04 +0500

    Counter Added

---
+++ useState/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  
+  return (
+    <>
+    <h1 className=''>Hello Welcome to This Simple Counter </h1>
+    </>
+  )
+}
+
+export default App

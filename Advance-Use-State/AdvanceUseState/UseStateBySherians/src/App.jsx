e51081ea62ca9865b975a0a9a224945e161e0997
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2026-08-03 15:07:16 +0500

    something went wrong...

--- Advance-Use-State/AdvanceUseState/UseStateBySherians/src/App.jsx
+++ Advance-Use-State/AdvanceUseState/UseStateBySherians/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+  const [num, setnum] = useState({user: "Huzaifa", age: 20})
+  const buttonClick = ()=>{
+    console.log(count.user);
+    const newNum = {...num}
+    newNum.user = "Hassan"
+    setnum(newNum)
+    
+    console.log(count.age);
+    
+
+  }
   return (
-    <div>App</div>
+      <>
+      <h1 className='mb-5 border-2 p-2 w-fit '>{count.user}, {count.age}</h1>
+      <button className='border-2 p-2' onClick={buttonClick}> Click</button>
+      </>
   )
+
 }
 
 export default App
